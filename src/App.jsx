69b18be3c2f37ea63be84a233137164b24b1def6
Jungwoo Hong <ghost1434@inu.ac.kr>
2025-03-05 16:03:10 +0900

    feat: mui, styled-component test

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import "./App.css";
 import AddBook from "./components/AddBook";
 import BooksList from "./components/BooksList";
 import Book from "./components/Book";
+import { Button } from "@mui/material";
+import BasicSpeedDial from "./components/Dial";
 
 function App() {
   return (
     <div>
-      <h1>graphQL 테스트를 해봐요!</h1>
+      <Button variant="outlined">graphQL 테스트를 해봐요!</Button>
       <BooksList />
       <Book id={1} />
       <Book id={2} />
       <AddBook />
+      <BasicSpeedDial />
     </div>
   );
 }
---
+++ src/components/Dial.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import PrintIcon from "@mui/icons-material/Print";
+import ShareIcon from "@mui/icons-material/Share";
+import styled from "styled-components";
+import { StyledEngineProvider } from "@mui/material";
+
+const actions = [
+  { icon: <FileCopyIcon />, name: "Copy" },
+  { icon: <SaveIcon />, name: "Save" },
+  { icon: <PrintIcon />, name: "Print" },
+  { icon: <ShareIcon />, name: "Share" },
+];
+
+export default function BasicSpeedDial() {
+  return (
+    <StyledEngineProvider injectFirst>
+      <MyBox sx={{ transform: "translateZ(0px)" }}>
+        <SpeedDial
+          ariaLabel="SpeedDial basic example"
+          sx={{ position: "absolute", bottom: 16, right: 16 }}
+          icon={<SpeedDialIcon />}
+        >
+          {actions.map((action) => (
+            <SpeedDialAction
+              key={action.name}
+              icon={action.icon}
+              tooltipTitle={action.name}
+            />
+          ))}
+        </SpeedDial>
+      </MyBox>
+    </StyledEngineProvider>
+  );
+}
+
+const MyBox = styled(Box)`
+  position: fixed;
+  top: 90%;
+  left: 90%;
+`;
